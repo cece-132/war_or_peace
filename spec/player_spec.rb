33a0df76a88b403350f3571ec45f4c3653609d82
Candace Eckels <candace.eckels@gmail.com>
2023-02-28 12:25:18 -0700

    Refactored has_lost? testing

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -31,31 +31,27 @@ RSpec.describe Player do
       end
 
       it 'has relationship to Card' do
-        @player.deck.cards each do |card|
+        @player.deck.cards.each do |card|
           expect(card).to be_a Card
         end
       end
     end
-    
+
   end
 
-  it "has_lost" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
+  describe '#has_lost?' do
+    it 'returns boolean whether a player has lost or not' do
+      expect(@player.has_lost?).to eq(false)
+    end
 
-
-    deck = Deck.new([card1, card2, card3])
-
-    player = Player.new('Clarisa', deck)
-
-    expect(player.has_lost?).to eq(false)
-
-    player.deck.remove_card
-    player.deck.remove_card
-    player.deck.remove_card
-
-    expect(player.has_lost?).to eq(true)
+    it 'if player loses all cards the player has lost' do
+      @player.deck.remove_card
+      @player.deck.remove_card
+      @player.deck.remove_card
+  
+      expect(@player.has_lost?).to eq(true)
+    end
   end
+
 
 end
