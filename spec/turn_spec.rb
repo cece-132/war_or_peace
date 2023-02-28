e1dce8b81762324595263009066ba0d54a0fd04d
Candace Eckels <candace.eckels@gmail.com>
2023-02-28 13:02:49 -0700

    Refactored type method for Turn class

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -3,71 +3,137 @@ require './lib/card'
 require './lib/deck'
 require './lib/player'
 require './lib/turn'
-require 'pry'
 
 RSpec.describe Turn do
-  it "exists" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
 
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
+  describe 'initialize(player1, player2)' do
 
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
+    before :each do
+      @card1 = Card.new(:diamond, 'Queen', 12)
+      @card2 = Card.new(:spade, '3', 3)
+      @card5 = Card.new(:heart, '8', 8)
+      @card8 = Card.new(:diamond, '2', 2)
+      
+      @card3 = Card.new(:heart, 'Ace', 14)
+      @card4 = Card.new(:diamond, 'Jack', 11)
+      @card6 = Card.new(:diamond, 'Queen', 12)
+      @card7 = Card.new(:heart, '3', 3)
+  
+      @deck1 = Deck.new([@card1, @card2, @card5, @card8])
+      @deck2 = Deck.new([@card3, @card4, @card6, @card7])
+  
+      @player1 = Player.new("Megan", @deck1)
+      @player2 = Player.new("Aurora", @deck2)
+  
+      @turn = Turn.new(@player1, @player2)
+    end
 
-    turn = Turn.new(player1, player2)
+    it 'exists' do
+      expect(@turn).to be_an_instance_of(Turn)
+    end
 
-    expect(turn).to be_an_instance_of(Turn)
+    it 'has attributes' do
+      expect(@turn.player1).to be_a Player
+      expect(@turn.player2).to be_a Player
+      expect(@turn.spoils_of_war).to be_a Array
+    end
   end
 
-  it "chooses a type" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
+  describe '#type' do
+    it 'returns :basic when players rank of card at index 0 are not the same' do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card5 = Card.new(:heart, '8', 8)
+      card8 = Card.new(:diamond, '2', 2)
+      
+      card3 = Card.new(:heart, 'Ace', 14)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+  
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card3, card4, card6, card7])
+  
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+  
+      turn = Turn.new(player1, player2)
 
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
+      expect(turn.type).to eq(:basic)
+    end
 
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
+    it 'returns :war when players rank of card at index 0 ar the same' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, 'Queen', 12)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
 
-    turn = Turn.new(player1, player2)
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
 
-    expect(turn.type).to eq(:basic)
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+      
+      turn = Turn.new(player1, player2)
+      expect(turn.type).to eq(:war)
+    end
+
+    it 'returns :mutually_assured_destruction when rank of card at index 0 and at index 2 are the same' do
+      card1 = Card.new(:heart, 'Jack', 11)
+      card2 = Card.new(:heart, '10', 10)
+      card3 = Card.new(:heart, '9', 9)
+      card4 = Card.new(:diamond, 'Jack', 11)
+      card5 = Card.new(:heart, '8', 8)
+      card6 = Card.new(:diamond, '8', 8)
+      card7 = Card.new(:heart, '3', 3)
+      card8 = Card.new(:diamond, '2', 2)
+
+      deck1 = Deck.new([card1, card2, card5, card8])
+      deck2 = Deck.new([card4, card3, card6, card7])
+
+      player1 = Player.new("Megan", deck1)
+      player2 = Player.new("Aurora", deck2)
+
+      turn = Turn.new(player1, player2)
+
+      expect(turn.type).to eq(:mutually_assured_destruction)
+    end
   end
 
-  it "chooses a type" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
-    card4 = Card.new(:diamond, 'Jack', 11)
-    card5 = Card.new(:heart, '8', 8)
-    card6 = Card.new(:diamond, 'Queen', 12)
-    card7 = Card.new(:heart, '3', 3)
-    card8 = Card.new(:diamond, '2', 2)
+  describe '#winner' do
+    describe 'a :basic turn type' do
+      it 'returns the player with the higher ranking card' do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)
+        card5 = Card.new(:heart, '8', 8)
+        card8 = Card.new(:diamond, '2', 2)
+        
+        card3 = Card.new(:heart, 'Ace', 14)
+        card4 = Card.new(:diamond, 'Jack', 11)
+        card6 = Card.new(:diamond, 'Queen', 12)
+        card7 = Card.new(:heart, '3', 3)
+    
+        deck1 = Deck.new([card1, card2, card5, card8])
+        deck2 = Deck.new([card3, card4, card6, card7])
+    
+        player1 = Player.new("Megan", deck1)
+        player2 = Player.new("Aurora", deck2)
+    
+        turn = Turn.new(player1, player2)
 
-    deck1 = Deck.new([card1, card2, card5, card8])
-    deck2 = Deck.new([card3, card4, card6, card7])
+        expect()
+      end
+    end
 
-    player1 = Player.new("Megan", deck1)
-    player2 = Player.new("Aurora", deck2)
-
-    turn = Turn.new(player1, player2)
-
-    expect(turn.type).to eq(:basic)
+    describe 'a :war turn type'
+    describe 'a :mutalluy_assured_destruction turn type'
   end
+
+
   it "chooses a winner" do
     card1 = Card.new(:heart, 'Jack', 11)
     card2 = Card.new(:heart, '10', 10)
