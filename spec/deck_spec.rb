595b3e7ac17baa3ccbfefecb6fc695251edcf058
Candace Eckels <candace.eckels@gmail.com>
2023-02-28 11:28:52 -0700

    Refactored initialize method for Deck class

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -4,21 +4,25 @@ require './lib/deck'
 
 RSpec.describe Deck do
 
-  describe 'initialize' do
-    it "exists and has attributes" do
-      deck = Deck.new(@cards)
-      expect(deck).to be_an_instance_of(Deck)
-    end
+  before :each do
+    @card1 = Card.new(:diamond, 'Queen', 12)
+    @card2 = Card.new(:spade, '3', 3)
+    @card3 = Card.new(:heart, 'Ace', 14)
+
+    @cards = [@card1, @card2, @card3]
+
+    @deck = Deck.new(@cards)
   end
 
-  it "has cards" do
-    card1 = Card.new(:diamond, 'Queen', 12)
-    card2 = Card.new(:spade, '3', 3)
-    card3 = Card.new(:heart, 'Ace', 14)
+  describe 'initialize(cards)' do
+    it "exists" do
+      expect(@deck).to be_an_instance_of(Deck)
+    end
 
-    deck = Deck.new([card1, card2, card3])
-
-    expect(deck.cards).to eq([card1, card2, card3])
+    it 'has attributes' do
+      expect(@deck.cards).to be_a Array
+      expect(@deck.cards.length).to eq(3)
+    end
   end
 
   it "has rank_of_card_at" do
